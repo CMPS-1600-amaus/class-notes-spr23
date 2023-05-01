ea40c2dbcbc6fbc8859ceaaac63be43f50265e46
Aaron Maus <arobie2@gmail.com>
2023-05-01 15:57:58 -0500

    Add list_sum_dc and binary_search

--- section-01/04-Haskell/00-source/funcs.hs
+++ section-01/04-Haskell/00-source/funcs.hs
@@ -13,3 +13,25 @@ list_sum :: (Num a) => [a] -> a
 list_sum [] = 0
 list_sum (x:[]) = x
 list_sum xs = head xs + list_sum (tail xs)
+
+list_sum_dc :: (Num a) => [a] -> a
+list_sum_dc [] = 0
+list_sum_dc (x:[]) = x
+list_sum_dc xs = (list_sum_dc left) + (list_sum_dc right)
+    where mid = (length xs) `div` 2
+          left = take mid xs
+          right = drop mid xs
+
+binary_search :: [Int] -> Int -> Bool
+binary_search [] k = False
+binary_search (x:[]) k = (if x == k then True else False)
+binary_search xs k = (if e == k 
+                        then True 
+                     else if k < e 
+                        then (binary_search left k) 
+                     else 
+                        binary_search right k)
+    where mid = (length xs) `div` 2
+          left = take mid xs
+          right = drop mid xs
+          e = xs !! mid
--- section-02/04-Haskell/00-source/funcs.hs
+++ section-02/04-Haskell/00-source/funcs.hs
@@ -9,3 +9,25 @@ list_sum :: (Num a) => [a] -> a
 list_sum [] = 0
 list_sum (x:[]) = x
 list_sum xs = head xs + list_sum (tail xs)
+
+list_sum_dc :: (Num a) => [a] -> a
+list_sum_dc [] = 0
+list_sum_dc (x:[]) = x
+list_sum_dc xs = (list_sum_dc left) + (list_sum_dc right)
+    where mid = (length xs) `div` 2
+          left = take mid xs
+          right = drop mid xs
+
+binary_search :: (Ord a) => [a] -> a -> Bool
+binary_search [] key = False
+binary_search (x:[]) key = (if x == key then True else False)
+binary_search xs key = (if e == key 
+                            then True 
+                        else if key < e 
+                            then binary_search left key
+                        else 
+                            binary_search right key)
+    where mid = (length xs) `div` 2
+          left = take mid xs
+          right = drop mid xs
+          e = xs !! mid
